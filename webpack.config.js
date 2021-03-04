3fabf422c5a026a543f02eb443a9e9d653c2db97
Bekzhan Borbekov <b.borbekov@kassa24.kz>
2021-03-04 11:00:06 +0600

    development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   module: {
     rules: [
